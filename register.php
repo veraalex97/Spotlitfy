1af6437a10225b8d6031b0f47a5da1b59a4e6914
Alex Vera <veraalex97@gmail.com>
2020-07-11 18:24:40 -0400

    added sanitization of POST data on register button.

--- register.php
+++ register.php
@@ -1,3 +1,39 @@
+<?php
+
+function sanitizeFormUsername($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+  $inputText = strip_tags($inputText);
+  $inputText = str_replace(" ", "", $inputText);
+  $inputText = ucfirst(strtolower($inputText));
+}
+
+function sanitizeFormPassword($inputText) {
+  $inputText = strip_tags($inputText);
+  return $inputText;
+}
+
+if (isset($_POST['loginButton'])) {
+  //echo "Login button was pressed.";
+}
+
+if (isset($_POST['registerButton'])) {
+  //on button press, grab username data and use sanitizing function
+  $username = sanitizeFormUsername($_POST['username']);
+  $firstName = sanitizeFormString($_POST['firstName']);
+  $lastName = sanitizeFormString($_POST['lastName']);
+  $email = sanitizeFormString($_POST['email']);
+  $email2 = sanitizeFormString($_POST['email2']);
+  $password = sanitizeFormPassword($_POST['password']);
+  $password2 = sanitizeFormPassword($_POST['password2']);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
