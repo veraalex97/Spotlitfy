dd01258b53392aa889be25a8e315eb9ef28ff1ea
Alex Vera <veraalex97@gmail.com>
2020-07-12 03:38:24 -0400

    login checker added

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -11,5 +11,7 @@ class Constants {
 	public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
 	public static $usernameCharacters = "Your username must be between 5 and 25 characters";
 	public static $usernameTaken = "Username already in use";
+
+	public static $loginFailed = "Your user or password is incorrect";
 }
 ?>
--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -1,6 +1,11 @@
 <?php
 if(isset($_POST['loginButton'])) {
-	//Login button was pressed
-	
+	$username = $_POST['loginUsername'];
+	$password = $_POST['loginPassword'];
+
+	$result = $account->login($username, $password);
+	if($result) {
+		header("Location: index.php");
+	}
 }
 ?>
