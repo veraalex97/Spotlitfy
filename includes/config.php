7cc85295da5589ff91bd1efda8138d8b682da0b2
Alex Vera <veraalex97@gmail.com>
2020-07-12 00:59:11 -0400

    db connection works, needed to add port

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 ob_start();
 $timezone = date_default_timezone_set("America/New_York");
 
-$con = mysqli_connect("localhost", "root", "password", "spotlitfy");
+$con = mysqli_connect("localhost:3306", "root", "password", "spotlitfy");
 
 if(mysqli_connect_errno()) {
   echo "error connecting to db" . mysqli_connect_errno();
